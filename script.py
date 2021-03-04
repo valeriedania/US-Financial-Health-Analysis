cb6d4370ce73dc32c5672efbcb878b358edf67f2
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-03-04 17:54:46 -0500

    Get NASDAQ data

--- script.py
+++ script.py
@@ -11,3 +11,6 @@ crude_oil_prices = pd.read_csv('crude_oil_prices.csv')
 #Create datetimes
 start = datetime(1999, 1,1)
 end = datetime(2019, 1,1)
+
+#Use web.DataReader to get historical prices
+nasdaq_data = web.DataReader("NASDAQ100", "fred", start, end)
