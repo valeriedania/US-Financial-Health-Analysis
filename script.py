fc18acf42f079cdcd8b78d0fd094eb74885bee2f
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-03-04 17:53:35 -0500

    Add module and create datetime

--- script.py
+++ script.py
@@ -1,6 +1,13 @@
 import seaborn as sns
 import pandas as pd
+import pandas_datareader.data as web
+from datetime import datetime
 
+#Import commodity prices
 gold_prices = pd.read_csv('gold_prices.csv')
 
 crude_oil_prices = pd.read_csv('crude_oil_prices.csv')
+
+#Create datetimes
+start = datetime(1999, 1,1)
+end = datetime(2019, 1,1)
